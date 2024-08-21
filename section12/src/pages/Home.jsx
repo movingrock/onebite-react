57bed27b77e185e2fac4df3c8fced60e99160506
movingrock <movingrock99@gmail.com>
2024-08-21 15:17:40 +0900

    feat: page title, favicon, thumbnail 구현

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 import { useContext, useState } from "react";
 import { DiaryStateContext } from "../App";
+import usePageTitle from "../hooks/usePageTitle";
 
 const getMonthlyData = (pivotDate, data) => {
   const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime();
@@ -13,8 +14,9 @@ const getMonthlyData = (pivotDate, data) => {
 
 const Home = () => {
   const data = useContext(DiaryStateContext);
-
   const [pivotDate, setPivotDate] = useState(new Date());
+  usePageTitle("감정 일기장");
+
   const monthlyData = getMonthlyData(pivotDate, data);
 
   const onIncreaseMonth = () => {
